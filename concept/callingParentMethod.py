b7db87474a309554f69baeb83baef898bb41c419
Vivek Gour <vivek@Viveks-MacBook-Air.local>
2018-05-02 18:42:50 +0530

    calling parent method from child using super

---
+++ concept/callingParentMethod.py
@@ -0,0 +1,29 @@
+__author__ = 'Vivek Gour'
+__copyright__ = 'Copyright 2018, Vivek Gour'
+__version__ = '1.0.0'
+__maintainer__ = 'Vivek Gour'
+__email__ = 'Viv30ek@searce.com'
+__status__ = 'Learning'
+
+
+class MyClassA(object):
+
+    def __init__(self, val):
+        self.val = val
+
+    def do_something(self):
+        print("print from A class")
+
+
+class MyClassB(MyClassA):
+
+    def __init__(self, val):
+        super(MyClassB, self).__init__(val)
+
+    def do_something(self):
+        print("print from B class")
+
+
+if __name__ == "__main__":
+    obj = MyClassB("vivek")
+    super(MyClassB, obj).do_something()
