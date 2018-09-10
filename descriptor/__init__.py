9d7e1a7ba664bacb85aa077f68824041ea8e40ac
Vivek Gour <Vivek.Gour@nike.com>
2018-09-10 18:39:16 +0530

    new code added

--- descriptor/__init__.py
+++ descriptor/__init__.py
@@ -1,8 +1,8 @@
 __author__ = 'Vivek Gour'
-__copyright__ = 'Copyright 2013, Searce'
+__copyright__ = 'Copyright 2013, Vivek Gour'
 __version__ = '1.0.0'
 __maintainer__ = 'Vivek Gour'
-__email__ = 'Vivek.Gour@searce.com'
+__email__ = 'viv30ek@gmail.com'
 __status__ = 'Development'
 
 
---
+++ interviewQus/sortList.py
@@ -0,0 +1,18 @@
+__author__ = 'Vivek Gour'
+__copyright__ = 'Copyright 2018, Vivek Gour'
+__version__ = '1.0.0'
+__maintainer__ = 'Vivek Gour'
+__email__ = 'Viv30ek@gmail.com'
+__status__ = 'Learning'
+
+"""
+    Sort list below list with 2nd element of inner list
+"""
+l = [[1, 2], [3, 3], [4, 1]]
+print (l)
+l.sort()
+print (l)
+l = [[1, 2], [3, 3], [4, 1]]
+
+print(sorted(l, key=lambda x: x[0]))
+print(sorted(l, key=lambda x: x[1]))
