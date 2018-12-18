55db3554856c4bda8f8f900a0dcfc65591dce653
Vivek Gour <viv30ek@gmail.com>
2018-12-18 10:24:08 +0530

    code added

---
+++ python_basics.py
@@ -0,0 +1,52 @@
+
+"""
+GIL
+
+1.    Keywords in Python programming language
+        False,	class,	finally,	is,	return
+        None,	continue,	for,	lambda,	try,
+        True,	def,	from,	nonlocal,	while,
+        and,	del,	global,	not,	with,
+        as,	elif,	if,	or,	yield,
+        assert,	else,	import,	pass,
+        break,	except,	in,	raise
+        
+2. Python Variables.
+    A = 1
+    A = "ASD"
+
+3. Python Data Types - Mutable and Immutable
+        Python Numbers - 1
+        Python List - []
+        [x*x for x in range(6)]
+        Python Tuple - ()
+        Python Strings - "" or ''
+        Python Set - {} & set()
+        Python Dictionary - {key: value}
+        {x: x*x for x in range(6)}
+
+        a = []
+
+
+4. Python Namespace and Scope
+
+5. if elif else
+
+6. for else
+
+7. while
+
+8. break and continue
+
+9. pass
+
+10. function
+
+11. Func Arguments 
+
+12. Python Anonymous/Lambda Function
+
+13. Class
+ 
+"""
+
